fcaf83846fa70070c8ced754f3a3ca0347bee7b5
vitonittwo <vorslvv33@inbox.lv>
2023-12-14 20:55:23 +0200

    Fixed date error

--- src/Main.java
+++ src/Main.java
@@ -10,9 +10,9 @@ public class Main {
         // char[] alphabetArr = {'а', 'б', "в", "г", "д", "е", "ж", "з", "и", "й", "к", "л", "м", "н", "о", "п", "р", "с", "т", "у", "ф", "х", "ц", "ч", "ш", "щ", "ъ", "ы", "ь", "э", "ю", "я", "?", ".", ",", " ", "\n"};
         // int[] days = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 67, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 53, 23, 24, 25, 26, 27, 28, 29, 30, 31};
         int[] codesAlphabet = {22, 15, 33, 45, 98, 27, 67, 90, 77, 43, 55, 82, 66, 91, 93, 81, 83, 56, 88, 35, 68, 11, 21, 14, 41, 23, 32, 57, 75, 19, 29, 44, 99, 72, 47, 74, 85,};
-        double[] functionsArg = {2.0, 3.0, 3.0, 3.0, 5.0, 4.0, 2.0, 10.0, 20.0, 8.0, 8.0, 4.0, 6.0, 10.0, 20.0, 5.0, 6.0, 7.0, 8.0, 7.0, 8.0, 1.0, 3.0, 4.0, 5.0, 7.0, 2.0, 4.0, 5.0, 10.0, 10.0};
+        double[] functionsArg = {2.0, 3.0, 3.0, 3.0, 5.0, 4.0, 2.0, 10.0, 20.0, 8.0, 8.0, 4.0, 6.0, 10.0, 20.0, 5.0, 6.0, 7.0, 8.0, 7.0, 8.0, 1.0, 3.0, 4.0, 5.0, 7.0, 2.0, 4.0, 5.0, 10.0, 10.0, 11.0};
         // char[] functions = {'2', '3', '3', '3', '5', '4', "2", "10", "20", "8", "8", "4", "6", "10", "20", "5", "6", "7", "8", "7", "8", "1", "3", "4", "5", "7", "2", "4", "5", "10", "10"};
-        String[] functions = {"+", "-", "/", "*", "+", "-", "/", "*", "+", "-", "+", "-", "/", "*", "+", "-", "/", "*", "+", "-", "/", "*", "+", "-", "/", "*", "+", "-", "/", "*", "+"};
+        String[] functions = {"+", "-", "/", "*", "+", "-", "/", "*", "+", "-", "+", "-", "/", "*", "+", "-", "/", "*", "+", "-", "/", "*", "+", "-", "/", "*", "+", "-", "/", "*", "+", "*"};
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Insert your text"); // Enter text for coding
@@ -26,10 +26,12 @@ public class Main {
         String currentDate = sc.nextLine(); // Enter the date of departure of the message
 
         int DayOfMonth = Integer.parseInt(currentDate.substring(0, 2));  // We allocate from the date the number of the day
-        int dataShiftResult = DayOfMonth + dataShift;  // We shift the date
+        int dataShiftResult = 0;  // We shift the date
         if (dataShiftResult > 31) {
             dataShiftResult = dataShiftResult - 31;
         }
+        else
+            dataShiftResult = DayOfMonth + dataShift;
         System.out.println(dataShiftResult);
 
         System.out.println(text);  // kontrole
